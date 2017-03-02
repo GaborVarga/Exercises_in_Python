82c9f9377461298823d2ba10585f3e5036f012ab
Gabor Varga <gabor.77.varga@gmail.com>
2017-03-02 13:58:50 +0100

    First commit.

---
+++ 46_exercises/1.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+#######################
+# Author: Gabor Varga #
+#######################
+
+# Exercise description :
+# 
+# Define a function maxi() that takes two numbers as arguments and returns 
+# the largest of them. Use the if-then-else construct available in Python. 
+
+# 
+# http://www.ling.gu.se/~lager/python_exercises.html
+
+import sys
+from pip._vendor.distlib.compat import raw_input
+
+###############
+# Function(s) #
+###############
+def maxi(n1, n2):
+    if n1 > n2:
+        print("The largest is",n1,"!")
+    else :
+        if n1 == n2 :
+            print("The numbers are equal.")
+        else :
+            print("The largest is",n2,"!")
+
+##########            
+# Script #
+##########
+
+okay = 0
+while okay != 1 :
+    try :
+        number1 = int(raw_input("Please enter the first number: \n"))
+        okay = okay + 1
+    except ValueError :
+        print("Sorry but this is not a number.\n")
+        okay = 0
+        continue
+okay = 0
+while okay != 1 :
+    try :
+        number2 = int(raw_input("Please enter another number: \n"))
+        okay = okay + 1
+    except ValueError :
+        print("Sorry but this is not a number.\n")
+        okay = 0
+        continue
+
+maxi(number1, number2)
+sys.exit()
