314968779eadef0e40abb417a5bcfe3ece775834
Gabor Varga <gabor.77.varga@gmail.com>
2017-03-03 12:56:48 +0100

    Version 1.0

--- 46_exercises/2.py
+++ 46_exercises/2.py
@@ -13,12 +13,56 @@
 # http://www.ling.gu.se/~lager/python_exercises.html
 
 import sys
+from pip._vendor.distlib.compat import raw_input
 
 ###############
 # Function(s) #
 ###############
 
-
+def maxi(numbers):    
+    m = -1
+    ii = 0
+    for i in range (len(numbers)):
+        if m < numbers[i]:
+            m = numbers[i]
+            ii = i
+    ii = ii + 1
+    print("The largest number's index is:",ii)
+    print("The largest number is:",m)
+    
 ##########            
 # Script #
 ##########
+
+okay = 0
+while okay != 1 :
+    try :
+        number1 = int(raw_input("Please enter the first number: \n"))
+        okay = okay + 1
+    except ValueError :
+        print("Sorry but this is not a number.\n")
+        okay = 0
+        continue
+okay = 0
+while okay != 1 :
+    try :
+        number2 = int(raw_input("Please enter the second number: \n"))
+        okay = okay + 1
+    except ValueError :
+        print("Sorry but this is not a number.\n")
+        okay = 0
+        continue
+okay = 0
+while okay != 1 :
+    try :
+        number3 = int(raw_input("Please enter the third number: \n"))
+        okay = okay + 1
+    except ValueError :
+        print("Sorry but this is not a number.\n")
+        okay = 0
+        continue
+    
+nums = [number1,number2,number3]
+maxi(nums)
+    
+sys.exit()
