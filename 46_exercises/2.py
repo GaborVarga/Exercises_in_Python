36ac0eebfb5088479db81e51223c226dcf7846ce
Gabor Varga <gabor.77.varga@gmail.com>
2017-03-03 11:02:45 +0100

    Second exercise, first draft.

---
+++ 46_exercises/2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+#######################
+# Author: Gabor Varga #
+#######################
+
+# Exercise No. 2
+# Description :
+# 
+# Define a function max_of_three() that takes three numbers 
+# as arguments and returns the largest of them.
+#
+# http://www.ling.gu.se/~lager/python_exercises.html
+
+import sys
+
+###############
+# Function(s) #
+###############
+
+
+##########            
+# Script #
+##########
