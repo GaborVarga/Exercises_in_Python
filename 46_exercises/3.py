72b4db3dec6f088aae484dcd27e3bdd75d54c319
Gabor Varga <gabor.77.varga@gmail.com>
2017-03-09 11:20:18 +0100

    Exercise number 3, final version.

---
+++ 46_exercises/3.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+#######################
+# Author: Gabor Varga #
+#######################
+
+# Exercise description :
+# 
+# Define a function that computes the length of a given list or string. 
+# (It is true that Python has the len() function built in, but writing it 
+# yourself is nevertheless a good exercise.)
+# 
+# http://www.ling.gu.se/~lager/python_exercises.html
+
+import sys
+from pip._vendor.distlib.compat import raw_input
+
+###############
+# Function(s) #
+###############
+
+
+##########            
+# Script #
+##########
+
+
+inputstring = raw_input("Please enter a string to determine its length: \n")
+
+i = 0
+for character in inputstring :
+    i = i +1
+    # print (character)
+print ("\nLength of string is: ",i)
