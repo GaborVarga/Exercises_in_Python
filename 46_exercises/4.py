ed1e7763372758fd96c8addd89bd3a4ff947eb03
Gabor Varga <gabor.77.varga@gmail.com>
2017-03-21 16:45:38 +0100

    Final version of this exercise.

--- 46_exercises/4.py
+++ 46_exercises/4.py
@@ -18,8 +18,21 @@ from pip._vendor.distlib.compat import raw_input
 # Function(s) #
 ###############
 
+def checkchar (c):
+    vowels = ['a','e','i','o','u']
+    if c in vowels:
+        print("\nThis is a vowel!")
+    else :
+        print("This is not a vowel.")
 
 ##########            
 # Script #
 ##########
 
+
+singlechar = raw_input("Please type 1 character: ")
+
+while len(singlechar) != 1 :
+    singlechar = raw_input("Please type only 1 character: ")
+
+checkchar(singlechar)
