e81a1e25eaa363ca1e54cce8eec5a9fc33e15dcd
Gabor Varga <gabor.77.varga@gmail.com>
2017-03-21 16:13:00 +0100

    First version of exercise 4.

---
+++ 46_exercises/4.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+#######################
+# Author: Gabor Varga #
+#######################
+
+# Exercise description :
+# 
+# Write a function that takes a character (i.e. a string of length 1) 
+# and returns True if it is a vowel, False otherwise.
+# 
+# http://www.ling.gu.se/~lager/python_exercises.html
+
+import sys
+from pip._vendor.distlib.compat import raw_input
+
+###############
+# Function(s) #
+###############
+
+
+##########            
+# Script #
+##########
+
